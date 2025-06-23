11bb3fdfb6a125fe40eefd2f4e8510516988769e
twinchutes <pete@twinchutes.io>
2025-06-23 16:21:38 -0600

    Finished Part 1

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 # %%
 import pandas as pd
-import numpy as np
+import joblib
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -8,20 +8,30 @@ from sklearn.naive_bayes import MultinomialNB
 # %%
 def read_data():
     data = pd.read_csv('IMDB Dataset.csv')
-    x = data[['review']].to_numpy()
-    y = data[['sentiment']].to_numpy()
+    x = data['review']
+    y = data['sentiment']
     return x, y
 
 # %%
-pipeline = Pipeline{[]}
-vectorizer = TfidfVectorizer(
-        sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english"
-    )
-clf = MultinomialNB()
+def train_model(x, y):
+    vectorizer = TfidfVectorizer(
+            sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english"
+        )
+    clf = MultinomialNB()
+    pipe = Pipeline(steps=[
+        ('vectorizer', vectorizer),
+        ('clf', clf)
+    ])
+    return pipe.fit(x, y)
+
+# %%
+def dump_to_file(pipeline_object):
+    joblib.dump(pipeline_object, 'sentiment_analysis.pkl')
 
 # %%
 if __name__ == '__main__':
     x, y = read_data()
-    
+    model = train_model(x, y)
+    dump_to_file(model)
 
 
