534b130aa6a7dc26eb5a9aef0d55f7d5150c5986
twinchutes <pete@twinchutes.io>
2025-06-23 16:10:00 -0600

    Testing modification workflow

--- train_model.py
+++ train_model.py
@@ -6,11 +6,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 # %%
-data = pd.read_csv('IMDB Dataset.csv')
-x = data[['review']].to_numpy()
-y = data[['sentiment']].to_numpy()
+def read_data():
+    data = pd.read_csv('IMDB Dataset.csv')
+    x = data[['review']].to_numpy()
+    y = data[['sentiment']].to_numpy()
+    return x, y
 
 # %%
+pipeline = Pipeline{[]}
+vectorizer = TfidfVectorizer(
+        sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english"
+    )
+clf = MultinomialNB()
 
+# %%
+if __name__ == '__main__':
+    x, y = read_data()
+    
 
 
